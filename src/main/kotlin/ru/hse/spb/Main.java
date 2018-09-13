0d928bbce8b95e4a825ce1f2fff132679662880a
subject-name-here <jura.paradov@gmail.com>
2018-09-13 18:20:34 +0300

    Project

--- src/main/kotlin/ru/hse/spb/Main.java
+++ src/main/kotlin/ru/hse/spb/Main.java
@@ -1,23 +1,23 @@
-//package ru.hse.spb;
-//
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
-//
-//public class Main extends Application {
-//
-//    @Override
-//    public void start(Stage primaryStage) throws Exception{
-//        Parent root = FXMLLoader.load(getClass().getResource("mainscreen.fxml"));
-//        primaryStage.setTitle("Blackjack");
-//        primaryStage.setScene(new Scene(root, 700, 700));
-//        primaryStage.show();
-//    }
-//
-//
-//    public static void main(String[] args) {
-//        launch(args);
-//    }
-//}
+package ru.hse.spb;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("mainscreen.fxml"));
+        primaryStage.setTitle("Blackjack");
+        primaryStage.setScene(new Scene(root, 700, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
